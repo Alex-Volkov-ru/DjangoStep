9d57c02da3debd6c3bddb1a51b683ee022cd5ad2
Your Name <you@example.com>
2024-06-16 12:45:38 +0300

    Add fist commit

---
+++ Course_Stepik/Course_Stepik/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Course_Stepik project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Course_Stepik.settings')
+
+application = get_wsgi_application()
