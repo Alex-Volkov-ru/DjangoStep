de25d25554cff5dde6edea9a7b1f6e8a2faff1aa
Your Name <you@example.com>
2024-07-02 08:30:55 +0300

    Add 2 lesson

--- Course_Stepik/women/urls.py
+++ Course_Stepik/women/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from women import views
+from women import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index),
-    path('cats/', views.categories),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path("archive/<year4:year>/", views.archive)
 ]
