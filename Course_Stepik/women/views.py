7fc4c555f4d28f72576c1003e57774a8ea536a4e
Ximikat <aleksandr.volkov1101@gmail.com>
2024-07-13 12:14:51 +0300

    Теги шаблоны

--- Course_Stepik/women/views.py
+++ Course_Stepik/women/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponsePermanentRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
 
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
@@ -23,6 +24,7 @@ def index(request):
         'set': {1, 2, 3, 4, 5, 6, 7},
         'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
         'obj': MyClass(10, 20),
+        'url': slugify("The Main Page")
         }
     return render(request,'women/index.html', data)
 
