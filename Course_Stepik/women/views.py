3487ce0bd26ef8b29332bc3c49e03d7d92014f2e
Your Name <you@example.com>
2024-07-15 20:05:18 +0300

    Блок пройден

--- Course_Stepik/women/views.py
+++ Course_Stepik/women/views.py
@@ -6,12 +6,11 @@ from django.template.defaultfilters import slugify
 
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
-
-# Create your views here.
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 def index(request):
     #t = render_to_string('women/index.html')
@@ -20,11 +19,7 @@ def index(request):
         'title' : 'Главная страница',
         'menu': menu,
         'float': 28.67,
-        'lst': [1, 2, 3, 5, 'abc', True],
-        'set': {1, 2, 3, 4, 5, 6, 7},
-        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-        'obj': MyClass(10, 20),
-        'url': slugify("The Main Page")
+        'post': data_db,
         }
     return render(request,'women/index.html', data)
 
