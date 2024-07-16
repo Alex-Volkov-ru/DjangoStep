6cb3f8bc1a3530a8f13419b6edd6c595ca3237dd
Ximikat <aleksandr.volkov1101@gmail.com>
2024-07-16 13:41:50 +0300

    Пройден блок

--- Course_Stepik/women/views.py
+++ Course_Stepik/women/views.py
@@ -5,7 +5,11 @@ from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
     {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
@@ -13,32 +17,27 @@ data_db = [
 ]
 
 def index(request):
-    #t = render_to_string('women/index.html')
-    #return HttpResponse(t)
     data = {
         'title' : 'Главная страница',
         'menu': menu,
-        'float': 28.67,
-        'post': data_db,
+        'posts': data_db,
         }
     return render(request,'women/index.html', data)
 
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте'})
+    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям<h1><p>id: {cat_id}</p>')
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id={post_id}")
 
-def categories_by_slug(request, cat_slug):
-    print(request.POST)
-    return HttpResponse(f'<h1>Статьи по категориям<h1><p>slug: {cat_slug}</p>')
+def add_page(request):
+    return HttpResponse('Добавление статьи')
 
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport',))
-        return HttpResponsePermanentRedirect(uri)
-        #raise Http404()
-    return HttpResponse(f'<h1>Архив по годам <h1><p> {year} </p>')
-    
+def contact(request):
+    return HttpResponse('Контакты')
+
+def login(request):
+    return HttpResponse('Авторизация')
+
 def page_not_found(request, exception):
     return  HttpResponseNotFound("<h1> Страница не найдена </h1>")
